d3baeab60bbd2c6150860a8c20a6617cc305c73a
Ben Szabo <me@benszabo.co.uk>
2024-12-04 22:03:34 +0000

    feat: 4th Dev - part 2

--- 04/main.go
+++ 04/main.go
@@ -56,22 +56,109 @@ func findXmas(matrix [][]string) (int, error) {
 	// check if the word "XMAS" appears in the matrix
 	// return the number of times the word "XMAS" appears in the matrix
 
-	totalOcurrences := 0
+	totalOccurrences := 0
 
 	for i, row := range matrix {
 		for j, char := range row {
 
 			if char == target1[0] {
 				start_x, start_y := i, j
-				totalOcurrences += checkMatrix(matrix, directions, row, target1, start_x, start_y)
+				totalOccurrences += checkMatrix(matrix, directions, row, target1, start_x, start_y)
 			}
 			if char == target2[0] {
 				start_x, start_y := i, j
-				totalOcurrences += checkMatrix(matrix, directions, row, target2, start_x, start_y)
+				totalOccurrences += checkMatrix(matrix, directions, row, target2, start_x, start_y)
 			}
 		}
 	}
-	return totalOcurrences, nil
+	return totalOccurrences, nil
+}
+
+type Direction string
+
+const (
+	Forward  Direction = "forward"
+	Backward Direction = "backward"
+)
+
+func isValidAxis(matrix [][]string, directions map[string][]int, direction Direction, row []string, start_x, start_y int) (string, string) {
+	x, y, a, b := 0, 0, 0, 0
+	switch direction {
+	case Forward:
+		x, y = start_x+directions["upRight"][0], start_y+directions["upRight"][1]
+		a, b = start_x+directions["downLeft"][0], start_y+directions["downLeft"][1]
+	case Backward:
+		a, b = start_x+directions["upLeft"][0], start_y+directions["upLeft"][1]
+		x, y = start_x+directions["downRight"][0], start_y+directions["downRight"][1]
+	}
+
+	if x < 0 || x >= len(matrix) || y < 0 || y >= len(row) {
+		return "", ""
+	}
+	if a < 0 || a >= len(matrix) || b < 0 || b >= len(row) {
+		return "", ""
+	}
+	topLetter := matrix[x][y]
+	bottomLetter := matrix[a][b]
+
+	return topLetter, bottomLetter
+}
+
+// findCrossMas returns the number of times the word "X-MAS" appears in the matrix
+// X shaped MAS, that is
+//
+// M.S
+// .A.
+// M.S
+func findCrossMas(matrix [][]string) (int, error) {
+	if matrix == nil {
+		return 0, errors.New("missing matrix")
+	}
+
+	directions := map[string][]int{
+		"upRight":   {-1, 1},  // diagonal up-right
+		"downRight": {1, 1},   // diagonal down-right
+		"upLeft":    {-1, -1}, // diagonal up-left
+		"downLeft":  {1, -1},  // diagonal down-left
+	}
+
+	totalXOccurrences := 0
+
+	// iterate through the matrix
+	for i, row := range matrix {
+
+		if i == 0 || i == len(matrix)-1 {
+			continue
+		}
+		for j, char := range row {
+			if j == 0 || j == len(row)-1 {
+				continue
+			}
+			if char == "A" {
+				upRightLetter, downLeftLetter := isValidAxis(matrix, directions, "forward", row, i, j)
+
+				if upRightLetter == downLeftLetter {
+					continue
+				}
+
+				if (upRightLetter == "M" && downLeftLetter == "S") || (upRightLetter == "S" && downLeftLetter == "M") {
+					downRightLetter, upLeftLetter := isValidAxis(matrix, directions, "backward", row, i, j)
+
+					if downRightLetter == upLeftLetter {
+						continue
+					}
+
+					if (downRightLetter == "M" && upLeftLetter == "S") || (downRightLetter == "S" && upLeftLetter == "M") {
+						totalXOccurrences++
+					}
+				}
+
+			}
+
+		}
+	}
+
+	return totalXOccurrences, nil
 }
 
 func main() {
@@ -86,9 +173,14 @@ func main() {
 			wordMatrix = append(wordMatrix, chars)
 		}
 	}
+	// task 1
 	number, err := findXmas(wordMatrix)
 	checkError(err)
+	fmt.Println("Task 1 solution: ", number)
 
-	fmt.Println(number)
+	// task 2
+	number, err = findCrossMas(wordMatrix)
+	checkError(err)
+	fmt.Println("Task 2 solution: ", number)
 
 }
--- 04/main_test.go
+++ 04/main_test.go
@@ -2,6 +2,41 @@ package main
 
 import "testing"
 
+func TestFindCrossMmas(t *testing.T) {
+	matrix := [][]string{
+		{"M", ".", "S"},
+		{".", "A", "."},
+		{"M", ".", "S"},
+	}
+
+	matrix2 := [][]string{
+		{"M", "M", "M", "S", "X", "X", "M", "A", "S", "M"},
+		{"M", "S", "A", "M", "X", "M", "S", "M", "S", "A"},
+		{"A", "M", "X", "S", "X", "M", "A", "A", "M", "M"},
+		{"M", "S", "A", "M", "A", "S", "M", "S", "M", "X"},
+		{"X", "M", "A", "S", "A", "M", "X", "A", "M", "M"},
+		{"X", "X", "A", "M", "M", "X", "X", "A", "M", "A"},
+		{"S", "M", "S", "M", "S", "A", "S", "X", "S", "S"},
+		{"S", "A", "X", "A", "M", "A", "S", "A", "A", "A"},
+		{"M", "A", "M", "M", "M", "X", "M", "M", "M", "M"},
+		{"M", "X", "M", "X", "A", "X", "M", "A", "S", "X"},
+	}
+
+	t.Run("finds XMAS", func(t *testing.T) {
+		want := 1
+		got, _ := findCrossMas(matrix)
+		if got != want {
+			t.Errorf("Expected %d, got %d", want, got)
+		}
+	})
+	t.Run("finds XMAS", func(t *testing.T) {
+		want := 9
+		got, _ := findCrossMas(matrix2)
+		if got != want {
+			t.Errorf("Expected %d, got %d", want, got)
+		}
+	})
+}
 func TestFindXmas(t *testing.T) {
 	matrix := [][]string{
 		{".", ".", "X", ".", ".", "."},
